b737f7b613b815288f84468fa6aaad2ac60a1b98
Markus Felten <Markus.Felten@gmx.de>
2017-01-08 19:01:37 +0100

    fix: use own tokenizer for identifiers with '-'

--- src/parser.js
+++ src/parser.js
@@ -3,7 +3,7 @@
 'use strict';
 
 import {
-  Parser, Tokenizer
+  Parser, Tokenizer, IdentifierToken
 }
 from 'pratt-parser';
 
@@ -17,69 +17,70 @@ function Value(value) {
 
 class NamedTokenizer extends Tokenizer {
 
-	makeIdentifier(chunk, offset, context, contextProperties) {
-		let i = offset;
-		i += 1;
-		for (;;) {
-			const c = chunk[i];
-			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
-				(c >= '0' && c <= '9') || c === '_' || c === '-') {
-				i += 1;
-			} else {
-				break;
-			}
-		}
+  makeIdentifier(chunk, offset, context, contextProperties) {
+    let i = offset;
+    i += 1;
+    for (;;) {
+      const c = chunk[i];
+      if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
+        (c >= '0' && c <= '9') || c === '_' || c === '-') {
+        i += 1;
+      } else {
+        break;
+      }
+    }
 
-		const value = chunk.substring(offset, i);
+    const value = chunk.substring(offset, i);
 
-		contextProperties.value = {
-			value: value
-		};
-		this._identifier(value, contextProperties, context);
-		return [Object.create(IdentifierToken, contextProperties), i - offset];
-	}
+    contextProperties.value = {
+      value: value
+    };
+    this._identifier(value, contextProperties, context);
+    return [Object.create(IdentifierToken, contextProperties), i - offset];
+  }
 }
 
 const grammar = {
-      identifier(value, properties, context) {},
-        prefix: {
-          '{': {
-            nud(grammar, left) {
-              const object = {};
+  prefix: {
+    '{': {
+      nud(grammar, left) {
+        const object = {};
 
-              if (grammar.token.value !== '}') {
-                do {
-                  const key = grammar.expression(0).value;
+        if (grammar.token.value !== '}') {
+          do {
+            const key = grammar.expression(0).value;
 
-                  /*
-                  if (grammar.token.value === ';') {
-                    object[key] = undefined;
-                    break;
-                  }*/
+            /*
+            if (grammar.token.value === ';') {
+              object[key] = undefined;
+              break;
+            }*/
 
-                  const value = grammar.expression(0).value;
+            const value = grammar.expression(0).value;
 
-                  object[key] = value;
-                  grammar.advance(';');
-                }
-                while (grammar.token.value !== '}');
-              }
-              grammar.advance('}');
-              return Value(object);
-            }
+            object[key] = value;
+            grammar.advance(';');
           }
-        },
-        infix: {
-          ';': {},
-          '}': {}
+          while (grammar.token.value !== '}');
         }
-    };
-    
+        grammar.advance('}');
+        return Value(object);
+      }
+    }
+  },
+  infix: {
+    ';': {},
+    '}': {}
+  }
+};
+
 /**
  * https://www.centos.org/docs/5/html/Deployment_Guide-en-US/s1-bind-namedconf.html
  */
 export class NamedParser extends Parser {
   constructor() {
-    super(grammar,{ tokenizer : new NamedTokenizer(grammar) });
+    super(grammar, {
+      tokenizer: new NamedTokenizer(grammar)
+    });
   }
 }
