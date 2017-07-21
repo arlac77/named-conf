047f6beacbd65abcd650b385b784c5a11862b202
Markus Felten <Markus.Felten@gmx.de>
2017-07-21 07:30:36 +0200

    chore: add missing rollup.config.js from template chore: add missing tests/rollup.config.js from template chore(devDependencies): add rollup-plugin-commonjs@^8.0.2 from template chore(devDependencies): add rollup-plugin-node-resolve@^3.0.0 from template chore(engines): update node@>=8.2.1 from template chore(npm): delete config chore(travis): merge from template .travis.yml chore(git): updated from template fix: add LICENSE

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,15 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default {
+  targets: [{
+    dest: pkg.main,
+    format: 'cjs'
+  }],
   plugins: [
     babel({
       babelrc: false,
       presets: ['stage-3'],
       exclude: 'node_modules/**'
     })
-  ],
-  targets: [{
-    dest: pkg.main,
-    format: 'cjs'
-  }],
-  external: ['pratt-parser']
+  ]
 };
