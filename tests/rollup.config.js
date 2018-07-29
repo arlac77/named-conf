62912dfa0fe3178e8dbf406ab4fe7ace3a3fda38
Markus Felten <Markus.Felten@gmx.de>
2018-07-29 16:48:54 +0200

    chore(rollup): reformat

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,15 +1,20 @@
 import istanbul from 'rollup-plugin-istanbul';
-
 import multiEntry from 'rollup-plugin-multi-entry';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+
 export default {
   input: 'tests/simple-test.js',
   external: ['ava', 'pratt-parser'],
 
-  plugins: [multiEntry(), istanbul({
-    exclude: ['tests/**/*-test.js']
-  }), resolve(), commonjs()],
+  plugins: [
+    multiEntry(),
+    istanbul({
+      exclude: ['tests/**/*-test.js']
+    }),
+    resolve(),
+    commonjs()
+  ],
 
   output: {
     file: 'build/test-bundle.js',
